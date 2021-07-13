5d5890ed4e256f5e5011bb4eba81067a8ec46e70
MoKihong <lionet2019@gmail.com>
2021-07-13 17:13:13 +0900

    업데이트

--- ProjectMovie/src/main/java/com/kh/projectMovie01/dao/NoticeBoardDao.java
+++ ProjectMovie/src/main/java/com/kh/projectMovie01/dao/NoticeBoardDao.java
@@ -7,5 +7,9 @@ import com.kh.projectMovie01.vo.NoticeBoardVo;
 public interface NoticeBoardDao {
 
 	public List<NoticeBoardVo> listAll();
-	
+	public NoticeBoardVo selectByBno(int b_no);
+	public int getNextVal();
+	public void insertArticle(NoticeBoardVo noticeBoardVo);
+	public void updateArticle(NoticeBoardVo noticeBoardVo);
+	public void deleteArticle(int b_no);
 }
