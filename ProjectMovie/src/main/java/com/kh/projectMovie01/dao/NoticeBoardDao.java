55b42c8d92312bc07f46b1043263e4ac293f85c6
MoKihong <lionet2019@gmail.com>
2021-07-15 17:15:43 +0900

    7/15

--- ProjectMovie/src/main/java/com/kh/projectMovie01/dao/NoticeBoardDao.java
+++ ProjectMovie/src/main/java/com/kh/projectMovie01/dao/NoticeBoardDao.java
@@ -3,13 +3,17 @@ package com.kh.projectMovie01.dao;
 import java.util.List;
 
 import com.kh.projectMovie01.vo.NoticeBoardVo;
+import com.kh.projectMovie01.vo.PagingDto;
 
 public interface NoticeBoardDao {
 
-	public List<NoticeBoardVo> listAll();
+	public List<NoticeBoardVo> listAll(PagingDto pagingDto);
 	public NoticeBoardVo selectByBno(int b_no);
 	public int getNextVal();
 	public void insertArticle(NoticeBoardVo noticeBoardVo);
 	public void updateArticle(NoticeBoardVo noticeBoardVo);
 	public void deleteArticle(int b_no);
+	public int getCount(PagingDto pagingDto);
+	public void updateViewCnt(int b_no);
+	public void updateCommentCnt(int b_no, int count);
 }
