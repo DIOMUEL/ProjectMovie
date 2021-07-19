2fa8b23b30b5734619c3f1c96159fa9d4c1e779a
DIOMUEL <rlaqjaals0213@gmail.com>
2021-07-19 17:31:12 +0900

    de

--- ProjectMovie/src/main/java/com/kh/projectMovie01/dao/Admin_AreaDao.java
+++ ProjectMovie/src/main/java/com/kh/projectMovie01/dao/Admin_AreaDao.java
@@ -1,6 +1,9 @@
 package com.kh.projectMovie01.dao;
 
+import com.kh.projectMovie01.vo.AreaVo;
+
 public interface Admin_AreaDao {
 
+	public AreaVo getAllAreaList();
 	public void movieAreaAdd(String area_name);
 }
